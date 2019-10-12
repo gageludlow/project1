4b2092e207be23a7b0abe077cc289b80f6c171f8
gageludlow <gageludlow@gmail.com>
2019-10-11 17:09:19 -0700

    finish recursive search and display directory

--- project1.py
+++ project1.py
@@ -1,10 +1,68 @@
 from pathlib import Path
 # p = Path('/Users/gageludlow/Projects')
 
-p = Path(input())
 
-#for child in p.iterdir():
-   # if child.is_dir:
-        
-    print(child)
+command = input()
+listStrings = command.split(' ')
+commandLetter = listStrings[0]
+userPath = Path(listStrings[1])
 
+def recursivePath(rPath):
+   currentFiles = []
+   for child in rPath.iterdir():
+      if child.is_dir():
+         currentFiles.append(child)
+         print(child)
+         recursivePath(child)
+      else:
+         currentFiles.append(child)
+         print(child)
+
+   return currentFiles   
+
+def printDirectories(dPath):
+   currentFiles = []
+   for child in dPath.iterdir():
+      if child.is_dir():
+         currentFiles.append(child)
+         print(f"if{child}")
+      else:
+         currentFiles.append(child)
+         print(f"else {child}")   
+      
+   return currentFiles
+
+
+if commandLetter == "D":
+   currentFiles = printDirectories(userPath)
+   secondCommand = input()
+   
+elif commandLetter == "R":
+   currentFiles = recursivePath(userPath)
+   secondCommand = input()
+else:
+   print("ERROR")
+
+
+
+ def getSecondInput(uInput, currentFiles):
+   listInput = uInput.split(' ')
+   commandLetter = listInput[0]
+   userPath = Path(listInput[1])
+   
+   if commandLetter == "A":
+       #do some
+       intFiles = currentFiles
+   elif commandLetter == "N":
+      #do some
+   elif commandLetter == "E":
+      #do some
+   elif commandLetter == "T":
+      #do some
+   elif commandLetter == "<"
+      #do some
+   elif commandLetter == ">"
+      #do some
+   else:
+      print("ERROR")
+      #reprompt
